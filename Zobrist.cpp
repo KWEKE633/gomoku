2a27d4cc0a427676f67dfae3ed5ee1be1e32b57b
QWERTOP18 <mz18yui@gmail.com>
2026-01-26 14:37:21 +0900

    feat: format

--- Zobrist.cpp
+++ Zobrist.cpp
@@ -3,18 +3,19 @@
 
 Zobrist::Zobrist()
 {
-	std::mt19937_64 rng(12345); // 　周期と範囲が広いため、ハッシュ化に適している
-	for (int y = 0; y < Config::BOARD_SIZE; ++y)
-	{
-		for (int x = 0; x < Config::BOARD_SIZE; ++x)
-		{
-			for (int p = 0; p < 3; ++p)
-			{
-				table[y][x][p] = rng();
-			}
-		}
-	}
-	turnHash = rng();
+    std::mt19937_64 rng(
+        12345); // 　周期と範囲が広いため、ハッシュ化に適している
+    for (int y = 0; y < Config::BOARD_SIZE; ++y)
+    {
+        for (int x = 0; x < Config::BOARD_SIZE; ++x)
+        {
+            for (int p = 0; p < 3; ++p)
+            {
+                table[y][x][p] = rng();
+            }
+        }
+    }
+    turnHash = rng();
 }
 
 const Zobrist zobrist;
